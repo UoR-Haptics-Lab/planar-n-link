577f177dad4b9c90526d06e312ecd8097f87ecae
Ozan Tokatli <ozan.tokatli@gmail.com>
2024-03-19 10:54:01 +0000

    prepared the script for matlab comparison

--- src/eom.py
+++ src/eom.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
 
     # Define link lengths and masses
     link_lengths = [symbols(f'l{i+1}') for i in range(num_dof)]
+    com_link_lengths = [symbols(f'lc{i+1}') for i in range(num_dof)]
     masses = [symbols(f'm{i+1}') for i in range(num_dof)]
 
     # Define inertias symbols
@@ -37,8 +38,8 @@ if __name__ == '__main__':
 
     # Define centre of mass (com) points on bodies
     com_points = []
-    for i, (link_length, frame, point),  in enumerate(zip(link_lengths, frames[1:], points[0:-1])):
-        com_points.append(point.locatenew(f'Po{i+1}', link_length / 2 * frame.x))
+    for i, (link_length, frame, point),  in enumerate(zip(com_link_lengths, frames[1:], points[0:-1])):
+        com_points.append(point.locatenew(f'Po{i+1}', link_length * frame.x))
 
     # Calculate velocities
     velocities = [points[0].set_vel(frames[0], 0 * frames[0].x + 0 * frames[0].y + 0 * frames[0].z)]
@@ -82,3 +83,30 @@ if __name__ == '__main__':
 
     # Gravity term
     gravity = Matrix([f.coeff(g) * g for f in forcing])
+
+    l, m, I = symbols('l m, I')
+    link_length_substitution = dict(zip(link_lengths + com_link_lengths, [l] * (len(link_lengths) + len(com_link_lengths))))
+    inertia_substitution = dict(zip([inertia_symbol[2] for inertia_symbol in inertia_symbols], [I] * len(inertia_symbols)))
+    mass_substitution = dict(zip(masses, [m] * len(masses)))
+    dummy_variables = dict(zip(generalized_positions + generalized_velocities + generalized_speed, [f'q{i+1}' for i in range(num_dof)] + [f'qp{i+1}' for i in range(num_dof)] + [f'u{i+1}' for i in range(num_dof)]))
+
+    print('End-effector position:')
+    x = points[-1].pos_from(points[0]).express(frames[0]).subs(link_length_substitution).simplify()
+    print(x.subs(dummy_variables))
+
+    print('End-effector velocity:')
+    xp = points[-1].vel(frames[0]).express(frames[0]).subs(link_length_substitution).simplify()
+    print(xp.subs(dummy_variables))
+
+    print('Mass matrix:')
+    for element in mass_matrix.subs(link_length_substitution).subs(mass_substitution).subs(dummy_variables).subs(inertia_substitution):
+      print(element.simplify())
+
+
+    print('Coriolis:')
+    C = coriolis.subs(link_length_substitution).subs(mass_substitution).subs(dummy_variables).subs(dict(zip(torques, [0] * len(torques))))
+    print([Ci.simplify() for Ci in C])
+
+    print('Gravity:')
+    G = gravity.subs(link_length_substitution).subs(dummy_variables)
+    print([Gi.simplify() for Gi in G])
